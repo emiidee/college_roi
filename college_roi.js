9f2e1a295fee7d78180b884ac2d161e83de98af7
emiidee <emily.y.dong@gmail.com>
2014-04-29 17:13:48 -0700

    Progressed on bar chart with test data

--- college_roi.js
+++ college_roi.js
@@ -1,4 +1,4 @@
-var d = [
+var data = [
   {"School": "Harvey Mudd College ", "Cost": "229500", "ROI": "980900"},
   {"School": "California Institute of Technology (Caltech) ", "Cost": "220400", "ROI": "837600"},
   {"School": "Massachusetts Institute of Technology (MIT) ", "Cost": "223400", "ROI": "831100"},
@@ -7,12 +7,28 @@ var d = [
   {"School": "Georgia Institute of Technology (In-State)", "Cost": "92250", "ROI": "755600"}
 ];
 
-d3.select(".chart")
-  .selectAll("div")
-    .data(d)
-  .enter().append("div")
-    .attr('class', 'bar')
-    .style("width", function(d) { return d.Cost / 300 + "px"; })
-    .style("height", '10px')
-    .text(function(d) { return d.School; });
-  
+var width = 420;
+    barHeight = 20;
+
+var x = d3.scale.linear()
+    .domain([0, d3.max(data.ROI)])
+    .range([0, width]);
+
+var chart = d3.select(".chart")
+    .attr("width", width)
+    .attr("height", barHeight * data.length);
+
+var bar = chart.selectAll("g")
+    .data(data)
+  .enter().append("g")
+    .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+bar.append("rect")
+    .attr("width", x)
+    .attr("height", barHeight - 1);
+
+bar.append("text")
+    .attr("x", function(d) { return x(d.ROI) - 3; })
+    .attr("y", barHeight / 2)
+    .attr("dy", ".35em")
+    .text(function(d) { return d; });
