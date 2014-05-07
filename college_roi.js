9fe04c880a0e6050665f0301027a3dba079a40af
emiidee <emily.y.dong@gmail.com>
2014-05-06 18:18:06 -0700

    Move old data files to archives

--- college_roi.js
+++ college_roi.js
@@ -1,17 +1,19 @@
-var data = [
-  {"School": "Harvey Mudd College ", "Cost": "229500", "ROI": "980900"},
-  {"School": "California Institute of Technology (Caltech) ", "Cost": "220400", "ROI": "837600"},
-  {"School": "Massachusetts Institute of Technology (MIT) ", "Cost": "223400", "ROI": "831100"},
-  {"School": "Stanford University ", "Cost": "236300", "ROI": "789500"},
-  {"School": "Colorado School of Mines (In-State)", "Cost": "114200", "ROI": "783400"},
-  {"School": "Georgia Institute of Technology (In-State)", "Cost": "92250", "ROI": "755600"}
-];
+// var data = [
+//   {"School": "Harvey Mudd College ", "Cost": "229500", "ROI": "980900"},
+//   {"School": "California Institute of Technology (Caltech) ", "Cost": "220400", "ROI": "837600"},
+//   {"School": "Massachusetts Institute of Technology (MIT) ", "Cost": "223400", "ROI": "831100"},
+//   {"School": "Stanford University ", "Cost": "236300", "ROI": "789500"},
+//   {"School": "Colorado School of Mines (In-State)", "Cost": "114200", "ROI": "783400"},
+//   {"School": "Georgia Institute of Technology (In-State)", "Cost": "92250", "ROI": "755600"}
+// ];
 
-var width = 420;
+var data = convertToObject(keys, rawData);
+
+var width = 500;
     barHeight = 20;
 
 var x = d3.scale.linear()
-    .domain([0, d3.max(data.ROI)])
+    .domain([0, d3.max(data, function(d){return +d.20netroi;})])
     .range([0, width]);
 
 var chart = d3.select(".chart")
@@ -24,11 +26,11 @@ var bar = chart.selectAll("g")
     .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
 
 bar.append("rect")
-    .attr("width", x)
+    .attr("width", function(d){ return x(+d.20netroi);})
     .attr("height", barHeight - 1);
 
 bar.append("text")
-    .attr("x", function(d) { return x(d.ROI) - 3; })
+    .attr("x", function(d) { return x(+d.20netroi) - 3; })
     .attr("y", barHeight / 2)
     .attr("dy", ".35em")
-    .text(function(d) { return d; });
+    .text(function(d) { return d.School; });
