3af9949afdaa2025e2bdc2ef0f4c2f69bb00fa88
emiidee <emily.y.dong@gmail.com>
2014-05-19 13:40:39 -0700

    Clean and comment existing code

--- static/college_roi.js
+++ static/college_roi.js
@@ -1,35 +1,31 @@
-    // var data = [
-//   {"School": "Harvey Mudd College ", "Cost": "229500", "ROI": "980900"},
-//   {"School": "California Institute of Technology (Caltech) ", "Cost": "220400", "ROI": "837600"},
-//   {"School": "Massachusetts Institute of Technology (MIT) ", "Cost": "223400", "ROI": "831100"},
-//   {"School": "Stanford University ", "Cost": "236300", "ROI": "789500"},
-//   {"School": "Colorado School of Mines (In-State)", "Cost": "114200", "ROI": "783400"},
-//   {"School": "Georgia Institute of Technology (In-State)", "Cost": "92250", "ROI": "755600"}
-// ];
+// Declare variables
 var width = 1000;
     barHeight = 20;
-
-var x = d3.scale.linear()
-    .range([0, width]);
 
 d3.csv('/data_clean.csv', function(error, data) {
     // todo: fix the max of x to match the current variable, keep in mind min
     // of negative values
-    x.domain([0, d3.max(data, function(d){return +d['20netroi'];})])
+    // Scale data range to fit in SVG object
+    var x = d3.scale.linear()
+        .range([0, width])
+        .domain([0, d3.max(data, function(d){return +d['20netroi'];})]);
 
+    // Set size of the SVG object with class .chart
     var chart = d3.select(".chart")
         .attr("width", width)
         .attr("height", barHeight * data.length);
 
+    // Put bars on the page
     var bar = chart.selectAll("g")
         .data(data)
       .enter().append("g")
         .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
-
+    // Size the height of the bars based on a data attribute
     bar.append("rect")
         .attr("width", function(d){ return x(d['20netroi_aid']);})
         .attr("height", barHeight - 1);
 
+    // Label bars
     bar.append("text")
         .attr("x", function(d) { return x(d['20netroi_aid']) - 3; })
         .attr("y", barHeight / 2)
