02076517e5f682683f3ad7e27bf2da5b93029d30
emiidee <emily.y.dong@gmail.com>
2014-05-19 20:53:08 -0700

    Convert data into nested object

--- static/college_roi.js
+++ static/college_roi.js
@@ -1,50 +1,83 @@
 // Declare variables
 var width = 1000;
-    barHeight = 20;
+  barHeight = 10;
 
-d3.csv('/data_clean.csv', function(error, data) {
-    // todo: fix the max of x to match the current variable, keep in mind min
-    // of negative values
-    // Scale data range to fit in SVG object
-    var x = d3.scale.linear()
-        .domain(d3.extent(data, function(d){return +d['20netroi_aid'];}))
-        .range([0, width]);
-        // .domain([0, d3.max(data, function(d){return +d['20netroi_aid'];})]);
+// Function to convert CSV data into JS object
+function type(d) {
+  return {
+    school: d['School'],
+    gradRate: parseFloat(d['Grad Rate']),
+    withoutAid: {
+      cost: parseInt(d['2013cost']),
+      netROI: majorData(parseInt, d, '_20netroi'),
+      annualROI: majorData(parseFloat, d, '_annualroi')
+    },
+    withAid: {
+      cost: parseInt(d['2013cost_aid']),
+      netROI: majorData(parseInt, d, '_20netroi_aid'),
+      annualROI: majorData(parseFloat, d, '_annualroi_aid')
+    },
+  };
+}
 
-    console.log(d3.extent(data, function(d){return +d['20netroi_aid'];}));
+// Helper function for data conversion
+function majorData(typeFunc, data, suffix) {
+  var result = {};
 
-    // Set size of the SVG object with class .chart
-    var chart = d3.select(".chart")
-        .attr("width", width)
-        .attr("height", barHeight * data.length);
+  for (var key in data) {
+    if(data.hasOwnProperty(key)) {
+      if(key.slice(-suffix.length) === suffix) { // if suffix of key is suffix
+        if (data[key] !== '') {
+          var major = key.slice(0, key.length - suffix.length);
+          result[major] = typeFunc(data[key]);
+        }
+      }
+    }
+  }
 
-    // Put bars on the page
-    var bar = chart.selectAll("g")
-        .data(data)
-      .enter().append("g")
-        .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
-    // Size the height of the bars based on a data attribute
-    bar.append("rect")
-        .attr("x", function(d) { return x(Math.min(0, d['20netroi_aid'])); })
-        .attr("width", 0)
-        .attr("height", barHeight - 1) // with gap
-    .transition().duration(800)
-        .ease(d3.ease('bounce'))
-        .attr("width", function(d){ return Math.abs(x(d['20netroi_aid']) - x(0));});
+  return result;
+}
 
-    // Label bars
-    bar.append("text")
-        .attr("x", function(d) { return x(d['20netroi_aid']) - 3; })
-        .attr("y", barHeight / 2)
-        .attr("dy", ".35em")
-        .text(function(d) { return d.School; });
-    
+d3.csv('/data.csv', type, function(error, data) {
+  console.log(data);
+  // todo: fix the max of x to match the current variable, keep in mind min
+  // of negative values
+  // Scale data range to fit in SVG object
+  var x = d3.scale.linear()
+      .domain(d3.extent(data, function(d){return +d['20netroi_aid'];}))
+      .range([0, width]);
+      // .domain([0, d3.max(data, function(d){return +d['20netroi_aid'];})]);
+
+  console.log(d3.extent(data, function(d){return +d['20netroi_aid'];}));
+
+  // Set size of the SVG object with class .chart
+  var chart = d3.select(".chart")
+    .attr("width", width)
+    .attr("height", barHeight * data.length);
+
+  // Put bars on the page
+  var bar = chart.selectAll("g")
+    .data(data)
+  .enter().append("g")
+    .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+  // Size the height of the bars based on a data attribute
+  bar.append("rect")
+    .attr("x", function(d) { return x(Math.min(0, d['20netroi_aid'])); })
+    .attr("width", 0)
+    .attr("height", barHeight - 1) // with gap
+  .transition().duration(800)
+    .ease(d3.ease('bounce'))
+    .attr("width", function(d){ return Math.abs(x(d['20netroi_aid']) - x(0));});
+
+  // Label bars
+  bar.append("text")
+    .attr("x", function(d) { return x(d['20netroi_aid']) - 3; })
+    .attr("y", barHeight / 2)
+    .attr("dy", ".35em")
+    .text(function(d) { return d.School; });
+  
 });
 
-// function type(d) {
-//   d.20netroi = +d.20netroi; // coerce to number
-//   return d;
-// }
 
 
 // Brainstorm
