9c51c0ecf4bef025de73273126ace7b323abec02
emiidee <emily.y.dong@gmail.com>
2014-05-19 19:07:46 -0700

    Clean folder structure

--- parser.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open('data_clean.csv', 'rU') as csvfile:
-    with open('data.js', 'w') as writer:
-        writer.write(
-            "var data = [\n    {0}\n];".format(
-                ",\n    ".join("[{0}]".format(",".join('"{0}"'.format(x) for x in row))
-                           for row in csv.reader(csvfile, delimiter=","))))
