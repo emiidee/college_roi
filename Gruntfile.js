a975b7693318c3a48306a160fc372a2ab3c259b8
emiidee <emily.y.dong@gmail.com>
2014-05-21 21:59:56 -0700

    Ported to using grunt for deployment.

---
+++ Gruntfile.js
@@ -0,0 +1,129 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      prod: {
+        options: {
+          separator: ';'
+        },
+        files: {
+          'public/dist/concat.js': ['public/client/**/*.js']
+        }
+      },
+      dev: {
+        options: {
+          separator: ';'
+        },
+        files: {
+          'public/dist/built.min.js': ['public/client/**/*.js']
+        }
+      }
+    },
+
+    nodemon: {
+      dev: {
+        script: 'server.js'
+      },
+      options: {
+        ignore: ['node_modules/**']
+      }
+    },
+
+    uglify: {
+      mainDist: {
+        files: {
+          'public/dist/built.min.js': ['public/dist/concat.js']
+        }
+      }
+    },
+
+    jshint: {
+      files: [
+        'public/client/**/*.js'
+      ],
+      options: {
+        force: 'true',
+        jshintrc: '.jshintrc',
+        ignores: [
+          'public/lib/**/*.js',
+          'public/dist/**/*.js',
+        ]
+      }
+    },
+
+    cssmin: {
+      minify: {
+        expand: true,
+        cwd: 'public/',
+        src: 'style.css',
+        dest: 'public/dist',
+        ext: '.min.css'
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: [
+          'public/client/**/*.js',
+          'public/lib/**/*.js',
+        ],
+        tasks: [
+          'jshint',
+          'browserify:dev',
+          'cssmin:minify'
+        ]
+      },
+      options: {
+        livereload: true
+      },
+      css: {
+        files: 'public/*.css',
+        tasks: ['cssmin']
+      }
+    },
+
+    browserify: {
+      main: {
+        src: ['public/client/**/*.js'],
+        dest: 'public/dist/concat.js'
+      },
+      dev: {
+        src: ['public/client/**/*.js'],
+        dest: 'public/dist/built.min.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-nodemon');
+
+  grunt.registerTask('server-dev', function (target) {
+    grunt.task.run([ 'nodemon' ]);
+  });
+
+  ////////////////////////////////////////////////////
+  // Main grunt tasks
+  ////////////////////////////////////////////////////
+
+  grunt.registerTask('build', [
+    'jshint',
+    'concat:prod',
+    'uglify',
+    'cssmin:minify'
+  ]);
+
+  grunt.registerTask('build-dev', [
+    'jshint',
+    'concat:dev',
+    'cssmin:minify'
+  ]);
+
+  grunt.registerTask('deploy', ['build', 'server-dev']);
+  grunt.registerTask('deploy-dev', ['build-dev', 'server-dev']);
+};
--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+var morgan = require('morgan'); 
 
+app.use(morgan({format: 'short'}));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
-app.use(express.static(__dirname + '/static'));
-
-app.get ('/', function(request, response) {
-  response.sendfile('index.html');
-  // response.render('index.html', function(err, html){
-  //   console.log('hello');
-  // });
-});
+app.use(express.static(__dirname + '/public'));
 
 app.get ('/data.csv', function(request, response) {
   response.sendfile('/data.csv');
