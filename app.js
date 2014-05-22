08271aee68c5cb447775785f6b29631116cd35bd
emiidee <emily.y.dong@gmail.com>
2014-05-21 20:55:04 -0700

    Add port variable for deployment

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use(express.static(__dirname + '/static'));
@@ -15,6 +16,6 @@ app.get ('/data.csv', function(request, response) {
   response.sendfile('/data.csv');
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(port, function() {
   console.log('Listening on port %d', server.address().port);
 });
