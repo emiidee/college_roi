993d9d14efe1b5a4a0afc2f830180d0ba0df8e13
emiidee <emily.y.dong@gmail.com>
2014-06-02 19:18:11 -0700

    Rebuild js code

--- public/dist/concat.js
+++ public/dist/concat.js
@@ -2,173 +2,97 @@
 var major = 'All';
 var aid = 'withoutAid';
 var roi = 'netROI';
+var schools = [];
+var cost = [];
+var colData = [];
 
 $(function () {
   // Load data using d3,see data_parse.js
   data
     .then(function(d){
-      // Get data
-      var colData = parseMajor(d);
-      var cost = parseCost(d);
+      d.sort(majorSort);
+      cost = parseCost(d);
+      colData = parseMajor(d);
+
+      // Set schools array for school search typeahead
+      schools = parseSchool(d);
+      setSchools(schools);
 
       // Append chart to div
-      var chart = c3.generate({
-        bindto: '#chart',
-        
-        data: {
-          columns: [
-            ['Cost'].concat(cost),
-            ['Major'].concat(colData),
-          ],
-          type: 'bar',
-          colors: {
-            Cost: '#ff7f0e',
-            Major: '#1f77b4'
-          },
-          groups: [
-            ['Major', 'Cost']
-          ],
-          names: {
-            Major: '20yr Net ROI'
-          }
-        },
-
-        transition: {
-          duration: 800
-        },
-
-        tooltip: {
-          format: {
-            title: function(i) { return d[i].school; },
-            value: function(value) {
-              var format = roi === 'netROI' ? d3.format('$,') : d3.format('.');
-                return format(value);
-            }
-          }
-        },
-
-        // Chart Formatting
-        grid: {
-          y: { // For negative values
-            lines: [{value: 0}]
-          }
-        },
-        axis: {
-          x: {
-            show: false,
-          },
-          y: {
-            show: true,
-            tick: {
-              format: d3.format('$,')
-            }
-          }
-        },
-        padding: {
-          bottom: 20
-        }
-      });
+      var chart = genChart(d, cost, colData);
 
       return [chart, d];
     })
-    .then(function(args) { // What is d?
+    .then(function(args) {
       var chart = args[0];
       var data = args[1];
+      var yText = 10;
 
       // Set listener for the major filter
-      $('#major-filter').submit(function(e) {
-        major = $('#active-major').val(); // Reset global major variable
-        // Reset financial 'aid' variable
-        // Reload data
-        var colData = parseMajor(data);
-        var cost = parseCost(data);
-        chart.load({
-          unload: ['Cost', 'Major'],
-          columns: [
-            ['Cost'].concat(cost),
-            ['Major'].concat(colData),
-          ],
-        });
-        $('#active-major').text(major + ' Majors');
+      $('#major-filter').change(function(e) {
         e.preventDefault();
+        // Reset global major variable
+        major = $('option:selected').text();
+        yText = 10;
+
+        // Filter undefined data
+        var d = _.filter(data, function(datum){
+          return datum[aid][roi][major] !== undefined;
+        });
+
+        // Sort data by major
+        d.sort(majorSort);
+        cost = parseCost(d);
+        colData = parseMajor(d);
+
+        // Reset typeahead list to current schools
+        schools = parseSchool(d);
+        setSchools(schools);
+
+        // Regenerate Chart
+        genChart(d, cost, colData);
+        $('#active-major').text(major + ' Majors');
       });
 
       // Listener to highlight current school
-      // $('#school-filter').submit(function(e) {
-      //   var school = $('#active-school').val();
-      //   chart.load({
-      //     unload: ['Cost', 'Major'],
-      //     columns: [
-      //       ['Cost'].concat(cost),
-      //       ['Major'].concat(colData),
-      //     ],
-      //   });
-      //   $('#active-major').text(major + ' Majors');
-      //   e.preventDefault();
-      // });
+      $('#school-filter').submit(function(e) {
+        e.preventDefault();
+        var school = $('#active-school').val();
+        var i = schools.indexOf(school);
+        var selector = '.c3-event-rect-' + i;
+        var x = d3.select(selector).attr('x');
+        var y = 0;
+        var w = d3.select(selector).attr('width');
+        var h = d3.select(selector).attr('height');
+        
+        d3.select('g').append('rect')
+          .attr('x', x)
+          .attr('y', y)
+          .attr('width', w)
+          .attr('height', h)
+          .style('fill', '#1f77b4')
+          .style('fill-opacity', '0.25');
+
+        d3.select('g').append('text')
+          .append('tspan')
+            .attr('x', x)
+            .attr('y', yText)
+            .text(school)
+          .append('tspan')
+            .attr('x', x)
+            .attr('y', yText + 15)
+            .text('Cost: ' + cost[i])
+          .append('tspan')
+            .attr('x', x)
+            .attr('y', yText + 30)
+            .text('ROI: ' + colData[i]);
+
+        yText += 50;
+      });
 
     });
 
-  var substringMatcher = function(strs) {
-    return function findMatches(q, cb) {
-      var matches, substringRegex;
-   
-      // an array that will be populated with substring matches
-      matches = [];
-   
-      // regex used to determine if a string contains the substring `q`
-      substrRegex = new RegExp(q, 'i');
-   
-      // iterate through the pool of strings and for any string that
-      // contains the substring `q`, add it to the `matches` array
-      $.each(strs, function(i, str) {
-        if (substrRegex.test(str)) {
-          // the typeahead jQuery plugin expects suggestions to a
-          // JavaScript object, refer to typeahead docs for more info
-          matches.push({ value: str });
-        }
-      });
-   
-      cb(matches);
-    };
-  };
-   
-  var majors = ['Art', 'Humanities', 'Computer Science', 'Mathematics', 'Business', 'Economics', 'Nursing', 'Life Sciences', 'Political Science', 'History', 'English', 'Education', 'All'
-  ];
-   
-  $('#major-filter .typeahead').typeahead({
-    hint: true,
-    highlight: true,
-    minLength: 1
-  },
-  {
-    name: 'majors',
-    displayKey: 'value',
-    source: substringMatcher(majors)
-  });
 });
-
-//***************************************************************
-// Helper Functions
-//***************************************************************
-
-function parseMajor(d) {
-  return _.chain(d)
-    .pluck(aid)
-    .pluck(roi)
-    .pluck(major)
-    .filter(function(val) { return val !== undefined; })
-    .value();
-}
-
-function parseCost(d) {
-  return _.chain(d)
-    .pluck(aid)
-    .filter(function(val) { return val[roi][major] !== undefined; })
-    .pluck('cost')
-    // .map(function(num) { return -num; }) // Make cost a negative value
-    .value();
-}
 
 
 //***************************************************************
@@ -179,8 +103,8 @@ function parseCost(d) {
 BASIC REQUIREMENTS
 [x] Convert to vertical bars
 [x] On hover of a bar, display attributes of that School
-[ ] Sort (auto or sort button?)
-[ ] Search for school, highlight that bar, use typeahead
+[x] Sort (auto or sort button?)
+[x] Search for school, highlight that bar, use typeahead
 [x] Filter by major data
 [x] Display cost as a stack
 
@@ -201,7 +125,152 @@ CLEAN UP TIME
 [ ] Push live
 [ ] Add readme
 */
-;var csv = Promise.promisify(d3.csv);
+;
+//***************************************************************
+// Helper Functions for college_roi.js
+//***************************************************************
+
+// Get schools list from data
+function parseSchool(d) {
+  return _.chain(d)
+    .pluck('school')
+    .filter(function(val) { return val !== undefined; })
+    .map(function(val, i) { return (i+1) + '. ' + val; })
+    .value();
+}
+
+// Get data for the selected major
+function parseMajor(d) {
+  return _.chain(d)
+    .pluck(aid)
+    .pluck(roi)
+    .pluck(major)
+    .filter(function(val) { return val !== undefined; })
+    .value();
+}
+
+// Get cost data
+function parseCost(d) {
+  return _.chain(d)
+    .pluck(aid)
+    .filter(function(val) { return val[roi][major] !== undefined; })
+    .pluck('cost')
+    // .map(function(num) { return -num; }) // Make cost a negative value
+    .value();
+}
+
+// Sort function for sorting by the current selected major
+function majorSort(a, b) {
+  var diff =  (b[aid][roi][major] + b[aid].cost) - (a[aid][roi][major] + a[aid].cost);
+  if (diff > 0) {
+    return 1;
+  } else if (diff < 0) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
+
+// Generate a chart and bind it to the html element #chart
+function genChart(d, cost, colData) {
+  c3.generate({
+    bindto: '#chart',
+    
+    data: {
+      columns: [
+        ['Cost'].concat(cost),
+        ['Major'].concat(colData),
+      ],
+      type: 'bar',
+      colors: {
+        Cost: '#ff7f0e',
+        Major: '#1f77b4'
+      },
+      groups: [
+        ['Major', 'Cost']
+      ],
+      names: {
+        Major: '20yr Net ROI'
+      }
+    },
+
+    transition: {
+      duration: 800
+    },
+
+    tooltip: {
+      format: {
+        title: function(i) { return (i+1) + '. ' + d[i].school; },
+        value: function(value) {
+          var format = roi === 'netROI' ? d3.format('$,') : d3.format('.');
+            return format(value);
+        }
+      }
+    },
+
+    // Chart Formatting
+    grid: {
+      y: { // For negative values
+        lines: [{value: 0}]
+      }
+    },
+    axis: {
+      x: {
+        show: false,
+      },
+      y: {
+        show: true,
+        tick: {
+          format: d3.format('$,')
+        }
+      }
+    },
+    padding: {
+      bottom: 20
+    }
+  });
+}
+
+// Typeahead
+function substringMatcher(strs) {
+  return function findMatches(q, cb) {
+    var matches, substringRegex;
+ 
+    // an array that will be populated with substring matches
+    matches = [];
+ 
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+ 
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        // the typeahead jQuery plugin expects suggestions to a
+        // JavaScript object, refer to typeahead docs for more info
+        matches.push({ value: str });
+      }
+    });
+ 
+    cb(matches);
+  };
+}
+
+// Set typeahead list for schools
+function setSchools(schools) {
+  $('#school-filter .typeahead').typeahead('destroy');
+
+  $('#school-filter .typeahead').typeahead({
+    hint: true,
+    highlight: true,
+    minLength: 1
+  },
+  {
+    name: 'schools',
+    displayKey: 'value',
+    source: substringMatcher(schools)
+  });
+};var csv = Promise.promisify(d3.csv);
 
 // Returns a promise of an array of objects
 var data = csv('/data.csv', type);
